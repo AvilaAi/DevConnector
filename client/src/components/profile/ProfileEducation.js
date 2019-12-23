1bb6427a8f26fdc986a3eb5efe7642f32d86910a
AvilaAi <avila911008@gmail.com>
2019-12-23 10:51:56 +0100

    bug fix

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+
 const ProfileEducation = ({ education: { school, degree, fieldofstudy, current, to, from, description } }) => (
 	<div>
 		<h3 className='text-dark'>{school}</h3>
